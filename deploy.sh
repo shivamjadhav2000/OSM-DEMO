3fcdd405c70e08406436c548cf27b0a9e8984401
shivamjadhav <shivam@cyrrup.com>
2023-01-23 16:46:07 +0530

    added git deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# abort on errorsset -e
+# buildyarn run build
+# navigate into the build output directorycd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git initgit add -A
+git commit -m 'deploy'
+git push -f git@github.com:shivamjadhav2000/OSM-DEMO.git master:gh-pages
+cd -
