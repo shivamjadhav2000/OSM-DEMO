ad3c7f716bd96e493048306748b2a4548df581d0
shivamjadhav <shivamjadhav348@gmail.com>
2023-12-31 01:28:44 -0600

    local

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <OSMMap />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import OSMMap from "@/components/OSMMap.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    OSMMap,
+  },
+};
+</script>
+<style>
+.home {
+  display: flex;
+  justify-content: center;
+}
+</style>
